a72b7a048c6ab427d880c1c8525d0b7baf9e2cc9
RyotaHirano <hirano@tsumikiinc.com>
2016-02-10 18:42:10 +0900

    add

--- gulp/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-import gulp from 'gulp';
-import del from 'del';
-import { clean as conf } from '../conf';
-
-gulp.task('clean', cb => {
-  del(conf.path).then(() => {
-    cb();
-  });
-});
--- gulp/tasks/serve.js
+++
@@ -1,7 +0,0 @@
-import gulp from 'gulp';
-import browserSync from 'browser-sync';
-import { serve as conf } from '../conf';
-
-gulp.task('serve', () => {
-  browserSync(conf);
-});
