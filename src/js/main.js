87cbb6918c9e33ff2ff4a528c36a448b76849d4d
RyotaHirano <hirano@tsumikiinc.com>
2016-03-03 18:51:23 +0900

    [del] 不要ファイル削除

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,7 @@
 import pathDispatcher from 'path-dispatcher';
-
-import commonAction from './common';
 import topAction from './top';
 
 const dispatcher = pathDispatcher({
-  '*': commonAction,
   '/': topAction
 }, {
   rootPath: '/alphabet-svg'
